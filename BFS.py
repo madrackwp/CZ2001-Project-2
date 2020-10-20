59f2703a85d8afb109928a3ba5c0f32314863506
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-10-20 17:07:21 +0800

    BFS algorithm is done

--- BFS.py
+++ BFS.py
@@ -1,4 +1,5 @@
 # adjacencyList will be a dictionary
+from readinput import readhospitals
 
 
 def BFS(adjacencyList, startingNode, noOfNodes, requiredHospitals, hospitalList):
@@ -8,28 +9,40 @@ def BFS(adjacencyList, startingNode, noOfNodes, requiredHospitals, hospitalList)
     currentDistance = 0
     # If the current node is a hospital, need to add the hospital along with the distance
 
-    queue[startingNode] = currentDistance
+    # This create an entry in the dictionary where the startingNode is the key, and the values are the key and the distance
+    queue[startingNode] = [startingNode, currentDistance]
     visited[startingNode] = True
     while (queue):
-        currentNode = queue.pop(startingNode)
-        if currentNode in hospitalList:  # This checks if the current node is a hospital
-            result[currentNode] = currentDistance
+        # This gets the first key in the queue and then removes it
+        res = list(queue.keys())[0]
+        currentNode = queue.pop(res)
+        nodeValue = currentNode[0]
+        currentDistance = currentNode[1]
+        if nodeValue in hospitalList:  # This checks if the current node is a hospital
+            # Stores the currentNode which is where the hospital, as the key, and the distance as the value
+            result[nodeValue] = currentDistance
 
         # checks for all neighbours of the current node
-        for neighbour in adjacencyList[currentNode]:
-            visited[neighbour] = True
-            queue[neighbour] = currentDistance+1
+        for neighbour in adjacencyList[nodeValue]:
+            if(visited[neighbour] == False):
+                visited[neighbour] = True
+                queue[neighbour] = [neighbour, currentDistance+1]
+        # BUG currentdistance should not increase until the we move on from the nxt layer
+        currentDistance += 1
 
     print(result)
 
 
-adjacencyList = {1: [2, 3, 4, 5, 6],
-                 2: [5],
-                 3: [],
+adjacencyList = {0: [1, 2, 3, 4, 5],
+                 1: [4],
+                 2: [],
+                 3: [5],
                  4: [6],
-                 5: [7],
-                 6: [7],
-                 7: []}
-hospitalList = [5, 7]
+                 5: [6, 7],
+                 6: [],
+                 7: [8],
+                 8: []}
+# hospitalList = [4, 6, 8]
+hospitalList = readhospitals()
 
-BFS(adjacencyList, 1, 7, 1, hospitalList)
+BFS(adjacencyList, 0, 9, 1, hospitalList)
--- readinput.py
+++ readinput.py
@@ -1,27 +1,28 @@
 def getGraph():
     graph = {}
-    with open('C:\\Users\\Jonathan Chang\\Documents\\Algo\\CZ2001-Project-2\\roadNet-TX.txt\\roadNet1000.txt', 'r') as f:
+    with open('C:\\Users\\madra\\Documents\\CZ2001\\CZ2001-Project-2\\hospitals.txt', 'r') as f:
         for line in f:
-                node = line.split()
-                graph.setdefault(node[0],[]).append(node[1])
+            node = line.split()
+            graph.setdefault(node[0], []).append(node[1])
     delete = [i for i in graph if i.isnumeric() is False]
     for i in delete:
         del graph[i]
     for i in graph:
         i = int(i)
-    return graph 
+    return graph
 
-print(getGraph())
+# print(getGraph())
+
 
 def readhospitals():
-    hospitals=[]
-    with open('C:\\Users\\Jonathan Chang\\Documents\\Algo\\CZ2001-Project-2\\hospitals.txt','r') as f:
+    hospitals = []
+    with open('C:\\Users\\madra\\Documents\\CZ2001\\CZ2001-Project-2\\hospitals.txt', 'r') as f:
         for line in f:
             try:
-                n=int(line)
+                n = int(line)
                 hospitals.append(n)
             except ValueError:
                 pass
     return hospitals
 
-print(readhospitals())
+# print(readhospitals())
