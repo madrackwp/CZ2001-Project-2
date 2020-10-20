b0bdede3fe72f21e181642116732ab0fa951a099
unknown <jonathanc1699@gmail.com>
2020-10-20 16:03:27 +0800

    Created readhospitals to read in text file

---
+++ readhospitals.py
@@ -0,0 +1,13 @@
+
+def readhospitals():
+    hospitals=[]
+    with open('C:\\Users\\Jonathan Chang\\Documents\\Algo\\CZ2001-Project-2\\hospitals.txt','r') as f:
+        for line in f:
+            try:
+                n=int(line)
+                hospitals.append(n)
+            except ValueError:
+                pass
+    return hospitals
+
+print(readhospitals())
