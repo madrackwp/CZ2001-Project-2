c1577032a4c967e043db8f6ecce91f3ed02b688b
notlynn002 <lynn.masillamoni@gmail.com>
2020-10-27 13:33:43 +0800

    function to store output of BFS results nicely in txt file

---
+++ output.py
@@ -0,0 +1,13 @@
+def output(num, dist, list):
+    f = open("C:\\Users\\lynnm\\Desktop\\AY20-21\\sem 1\\CZ2001 algo\\proj 2 code\\output.txt","a")
+    f.write("start node: ")
+    f.write(str(num))
+    f.write(", shortest dist: ")
+    f.write(str(dist))
+    f.write(", shortest path: ")
+    f.write("[")
+    for i in range(len(list)):
+        f.write(str(list[i]))
+        f.write(",")
+    f.write("]\n")
+    f.close()
