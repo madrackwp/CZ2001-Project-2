8096ec824c4e63d7bd5ea5b7a58b90aa9bd3a9ab
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-10-29 23:02:48 +0800

    Editted the README file

--- Interface.py
+++ Interface.py
@@ -5,13 +5,13 @@ from WriteToOP import wipeTextFile
 
 
 # =======================================PATHS TO BE CHANGED=======================================
-graphPath = 'C:\\Users\\madra\\Documents\\CZ2001\\CZ2001-Project-2\\Paths\\roadNet-TX.txt'
+graphPath = 'C:\\Users\\madra\\Documents\\CZ2001\\CZ2001-Project-2\\Paths\\randomGraph.txt'
 opPath = "C:\\Users\\madra\\Documents\\CZ2001\\CZ2001-Project-2\\Outputs\\Output.txt"
 hopsitalPath = 'C:\\Users\\madra\\Documents\\CZ2001\\CZ2001-Project-2\\Paths\\hospitals.txt'
 # =================================================================================================
 
 # =======================================VARS TO BE CHANGED========================================
-requiredHospitals = 1
+requiredHospitals = 2
 # =================================================================================================
 
 # =====================DO NOT CHANGE ANYTHING AT THE BOTTOM OF THIS LINE===========================
